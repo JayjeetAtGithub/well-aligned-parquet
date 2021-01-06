77f0b6515cf6a53abfdc353b8f9988321769a490
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-01-06 14:40:39 +0530

    Update checker.py

--- checker.py
+++ checker.py
@@ -13,19 +13,6 @@ meta = pq.read_metadata(FILENAME)
 print("Metadata: ", meta)
 
 rg_padding = 0
-for i in range(0, meta.num_row_groups-1):
-	rg_padding = meta.row_group(i+1).column(0).data_page_offset -  meta.row_group(i).column(meta.num_columns-1).file_offset
-	print("RowGroup Padding: ", rg_padding)
-
-total_rg_size = 0
 for i in range(0, meta.num_row_groups):
-	total_rg_size += meta.row_group(i).total_byte_size
+	print("RowGroup row count: ", meta.row_group(i).num_rows)
 
-
-HEADER = 4
-FOOTER_MAGIC = 4
-
-total = HEADER + (total_rg_size + (rg_padding * (meta.num_row_groups-1)))  + meta.serialized_size + 4 + FOOTER_MAGIC
-
-print(total)
-print(file_size)
